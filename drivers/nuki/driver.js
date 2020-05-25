74d4cc611025538cacbd0bde62a4d7f874fe2674
jghaanstra <jghaanstra@gmail.com>
2020-05-25 22:03:46 +0200

    Release 1.2.0

--- drivers/nuki/driver.js
+++ drivers/nuki/driver.js
@@ -16,37 +16,39 @@ class NukiDriver extends Homey.Driver {
       let devices = [];
       try {
         let bridgeList = await util.sendCommand('https://api.nuki.io/discover/bridges', 4000);
-        for (let i in bridgeList.bridges) {
-          var authpath = 'http://'+ bridgeList.bridges[i].ip +':'+ bridgeList.bridges[i].port +'/auth';
-          var auth = await util.sendCommand(authpath, 32000);
-          if (auth.success == true) {
-            var locklistpath = 'http://'+ bridgeList.bridges[i].ip +':'+ bridgeList.bridges[i].port +'/list?token='+ auth.token;
-            var deviceList = await util.sendCommand(locklistpath, 2000);
-            if (deviceList) {
-              for (let x in deviceList) {
-                devices.push({
-                  name: deviceList[x].name +' ('+ bridgeList.bridges[i].ip +')',
-                  data: {
-                    id: deviceList[x].nukiId
-                  },
-                  settings: {
-                    address: bridgeList.bridges[i].ip,
-                    port: bridgeList.bridges[i].port,
-                    nukiId: deviceList[x].nukiId,
-                    token: auth.token
-                  }
-                });
+        if (JSON.stringify(bridgeList).includes('"ip":')) {
+          for (let i in bridgeList.bridges) {
+            var authpath = 'http://'+ bridgeList.bridges[i].ip +':'+ bridgeList.bridges[i].port +'/auth';
+            var auth = await util.sendCommand(authpath, 32000);
+            if (auth.success == true) {
+              var locklistpath = 'http://'+ bridgeList.bridges[i].ip +':'+ bridgeList.bridges[i].port +'/list?token='+ auth.token;
+              var deviceList = await util.sendCommand(locklistpath, 2000);
+              if (deviceList) {
+                for (let x in deviceList) {
+                  devices.push({
+                    name: deviceList[x].name +' ('+ bridgeList.bridges[i].ip +')',
+                    data: {
+                      id: deviceList[x].nukiId
+                    },
+                    settings: {
+                      address: bridgeList.bridges[i].ip,
+                      port: bridgeList.bridges[i].port,
+                      nukiId: deviceList[x].nukiId,
+                      token: auth.token
+                    }
+                  });
+                }
               }
+            } else {
+              callback(Homey.__("Did not receive a token from the bridge, make sure you push the bridge button during pairing."), false);
             }
-          } else {
-            callback(Homey.__("Did not receive a token from the bridge, make sure you push the bridge button during pairing."), false);
           }
-        }
-        if (devices.length) {
           callback(null, devices);
         } else {
           socket.showView('select_pairing');
         }
+
+
       } catch(error) {
         callback(error, false);
       }
